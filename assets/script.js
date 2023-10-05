a638ab89dad361c78f2084992b3f85d00dc444b3
stevensillsii <stevensills2@gmail.com>
2023-10-04 22:05:53 -0700

    working on the ClearAll button and playing with the time

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 // displays the time
 var today = dayjs();
-$('#1a').text(today.format('h:mm A dddd MMM D, YYYY'));
+$('#1a').text(today.format('hh:mm A dddd MMM D, YYYY'));
+
+// var todayDate = dayjs().format('dddd, MMM Do YYYY');
+// $("#1a").html(todayDate);
 
 
 // wraps the code so that nothing runs before everythign is loaded
@@ -13,19 +16,23 @@ $(document).ready(function() {
     var time = $(this).parent().attr("id");
 
 // this will save the user data to local storage
-    localStorage.setItem(time, text);
-  });
+    localStorage.setItem(text, time);
+    console.log(text)
+    console.log(time)
+  })
 
 // button to clear the fields and the local storage
-$('#clearFieldsBtn').click(function (event) {
+$("#clearAll").click(function (event) {
   event.preventDefault;
-  $('textarea').val("");
+  $("description").val("");
   localStorage.clear();
-});
+})
 
+function timeTracker() {
+  var timeNow = dayjs().hour();
 
-$('.time-block').each(function () {
-  var blockTime = parseInt($(this).attr('id').split('hour')[1]);
+$(".time-block").each(function () {
+  var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
 // adds or removes the class depending on the time
 // this is what will determine what color the blocks are
@@ -44,38 +51,18 @@ else {
   $(this).removeClass('past');
   $(this).addClass('future');
 }
+})
+}
+//retrieves items from local storage and sets them in proper places
+$("#hour-09 .time-block").val(localStorage.getItem("09"));
+$("#hour-10 .time-block").val(localStorage.getItem("10"));
+$("#hour-11 .time-block").val(localStorage.getItem("11"));
+$("#hour-12 .time-block").val(localStorage.getItem("12"));
+$("#hour-1 .time-block").val(localStorage.getItem("01"));
+$("#hour-2 .time-block").val(localStorage.getItem("02"));
+$("#hour-3 .time-block").val(localStorage.getItem("03"));
+$("#hour-4 .time-block").val(localStorage.getItem("04"));
+$("#hour-5 .time-block").val(localStorage.getItem("05"));
 
-});
-
-// // Saves the notes to local storage
-// function saveNotes() {
-//   localStorage.setItem("myDay", JSON.stringify(myDay));
-// }
-// // pulls the notes back in from local storage
-// function displayNotes() {
-//   myDay.forEach(function (_thisHour) {
-//     $('#${_thisHour.id}').val(_thisHour.reminder)
-//   });
-// }
-
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+timeTracker();
+})
