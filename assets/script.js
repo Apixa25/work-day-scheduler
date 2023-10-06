691c2b235ed9cd4f3abefa96e839956bc52c77e4
stevensillsii <stevensills2@gmail.com>
2023-10-05 19:56:38 -0700

    starting the README

--- assets/script.js
+++ assets/script.js
@@ -25,12 +25,12 @@ $(".saveBtn").on("click", function () {
 // button to clear the fields and the local storage
 $("#clearAll").click(function (event) {
   event.preventDefault;
-  $("description").val("");
+  $(".description").val("");
   localStorage.clear();
 })
 
 // this is the function that changed the colors of the time blocks
-function timeTracker() {
+function colorPlacer() {
   var timeNow = dayjs().hour();
 
 $(".time-block").each(function () {
@@ -57,16 +57,16 @@ else {
 }
 
 //retrieves items from local storage and sets them in proper places
-$("#hour-9 .description").val(localStorage.getItem("hour9"));
-$("#hour-10 .description").val(localStorage.getItem("hour10"));
-$("#hour-11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("12"));
-$("#hour1 .description").val(localStorage.getItem("01"));
-$("#hour-2 .description").val(localStorage.getItem("02"));
-$("#hour-3 .description").val(localStorage.getItem("03"));
-$("#hour-4 .description").val(localStorage.getItem("04"));
-$("#hour-5 .description").val(localStorage.getItem("05"));
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-1 .description").val(localStorage.getItem("hour-01"));
+$("#hour-2 .description").val(localStorage.getItem("hour-02"));
+$("#hour-3 .description").val(localStorage.getItem("hour-03"));
+$("#hour-4 .description").val(localStorage.getItem("hour-04"));
+$("#hour-5 .description").val(localStorage.getItem("hour-05"));
 
-timeTracker();
+colorPlacer();
 })
 
